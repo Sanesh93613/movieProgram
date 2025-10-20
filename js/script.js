f834910a7968aa5fa3eb8170b688fb03bcdeea14
Alexandr Sharudenko <rabota.it.2002@gmail.com>
2025-10-20 03:45:18 +0300

    added loop for a questions

--- js/script.js
+++ js/script.js
@@ -1,43 +1,33 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// 4) Потренироваться и переписать цикл еще двумя способами*/
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
 const personalMovieDB = {
-    count: numberOfFilms, // сюда передается ответ на первый вопрос
-    movies: {}, // в это свойство поместить пустой объект
-    actors: {}, //тоже поместить пустой объект
-    genres: [], //сюда поместить пустой массив
-    privat: false, // в это свойство поместить boolean(логическое) значение false
+    count: numberOfFilms,
+    movies: [],
+    actors: {},
+    genres: [],
+    privat: false,
 };
 
-const firstLastFilm = prompt("Один из последних просмотренных фильмов?", ""),
-    firstFilmRaiting = +prompt("На сколько оцените его?", ""),
-    secondLastFilm = prompt("Один из последних просмотренных фильмов?", ""),
-    secondFilmRaiting = +prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+    let lastFilm = prompt("Один из последних просмотренных фильмов?");
+    let filmRaiting = prompt("На сколько оцените его?");
 
-personalMovieDB.movies[firstLastFilm] = firstFilmRaiting;
-personalMovieDB.movies[secondLastFilm] = secondFilmRaiting;
-
-console.log(personalMovieDB);
+    if (lastFilm && filmRaiting && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = filmRaiting;
+    } else {
+        i = 0;
+    }
+}
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    ("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
